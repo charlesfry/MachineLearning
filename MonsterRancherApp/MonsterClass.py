623e786d3512756e3592a0b485c2f15434ce6d21
Charles Fry <52051562+AmishWarlord@users.noreply.github.com>
2020-09-20 22:00:06 -0400

    Update MonsterClass.py

--- MonsterRancherApp/MonsterClass.py
+++ MonsterRancherApp/MonsterClass.py
@@ -31,19 +31,31 @@ class Monster_Creator(Monster) :
 
     @staticmethod
     def draw_sample(mean,std):
-        return np.random.normal(loc=mean,scale=std,size=(1,1))
+        sample = np.random.normal(loc=mean,scale=std,size=(1,1))[0][0]
+
+        # limit increase/decrease to 5 * standard deviation
+        sample = max(sample, mean - 5*std)
+        sample = min(sample, mean + 5*std)
+        return int(np.round(sample))
 
     def create_monster(self,seed):
         self.seed_everything(seed)
         name = self.name
         type1 = self.type1
         type2 = self.type2
+        hp = self.draw_sample(*self.health)
+        str = self.draw_sample(*self.str)
+        dfn = self.draw_sample(*self.dfn)
+        dex = self.draw_sample(*self.dex)
+        int = self.draw_sample(*self.int)
+        agil = self.draw_sample(*self.agil)
+        chr = self.draw_sample(*self.chr)
 
-
-
+        return Monster(name,type1,type2,hp,str,dfn,dex,int,agil,chr)
 
 
 
 if __name__ == '__main__' :
-    mon = Monster_Creator('Fuffmup','Woodlander','None',(100,5),(10,2),(6,1),(14,1.5),(11,.78),(16,3),(19,1))
-    butt = mon.create_monster()
+    cr = Monster_Creator('Fuffmup','Woodlander','None',(100,5),(10,2),(6,1),(14,1.5),(11,.78),(16,3),(19,1))
+    mon = cr.create_monster(seed=0)
+    print(vars(mon))
