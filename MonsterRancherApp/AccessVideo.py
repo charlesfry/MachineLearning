c6ae9667b8b57cc374ecbc09436833105a6f1136
Charles Fry <52051562+AmishWarlord@users.noreply.github.com>
2020-09-22 20:49:36 -0400

    Update AccessVideo.py

--- MonsterRancherApp/AccessVideo.py
+++ MonsterRancherApp/AccessVideo.py
@@ -3,11 +3,20 @@ from imutils.video import VideoStream
 from pyzbar import pyzbar
 import argparse
 import datetime
-from time import time
+import time
 import cv2 as cv
 
 # construct parser and parse args
 parser = argparse.ArgumentParser()
-parser.add_argument('--o','output',type=str,default='barcodes.csv',help='path for csv outfile')
+default_path = 'barcodes.csv'
+parser.add_argument('--o','output',type=str,default=default_path,help='path for csv outfile')
 args = vars(parser.parse_args())
 
+# initialize video stream and allow the camera to warm up
+print('[INFO] starting video stream...')
+vs = VideoStream(usePiCamera=True).start()
+time.sleep(2.0)
+
+# open the outfile
+csv = open(args['output'],'w+')
+found = set()
