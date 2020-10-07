4ea10f52767e4fa1c3a5c89e088db65fd4c68a2d
Charles Fry <AmishWarlord@users.noreply.github.com>
2020-10-07 17:56:29 -0400

    Update psnr.py

--- Upsampler/psnr.py
+++ Upsampler/psnr.py
@@ -2,11 +2,19 @@ from warnings import warn
 import numpy as np
 
 
-def psnr(img1, img2):
+def psnr(img1:np.array, img2:np.array):
+    """
+    inputs: two numpy arrays representing pixel values of images
+
+    returns: the PSNR score of the images' similarity
+    """
+
+
     assert img1.shape == img2.shape, f'Images must be of the same shape. You passed images of shapes {img1.shape},{img2.shape}'
     shape = list(img1.shape)
+    assert len(shape) > 0
 
-    if len(img1.shape) != 2:
+    if len(img1.shape) != 2 and len(img1.shape) != 3:
         print(f'[WARNING] input shape is of dimension {len(img1.shape)}. Did you mean to do this?')
 
     assert shape is not None, f'shape of image must be 2-dimensional. Shape: {img1.shape}'
@@ -29,8 +37,8 @@ def psnr(img1, img2):
 
 
 if __name__ == '__main__':
-    img1 = np.random.randn(64, 64)
-    img2 = np.random.randn(64, 64)
+    img1 = np.random.randn(64, 64,3,5)
+    img2 = np.random.randn(64, 64,3,5)
 
     print(psnr(img1, img2))
-    print(psnr(img1, img1.copy()))
+
