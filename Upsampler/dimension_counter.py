1c1001fb2dff36a52a0778f72be48a1393a22b88
Charles Fry <AmishWarlord@users.noreply.github.com>
2020-10-08 08:34:19 -0400

    Create dimension_counter.py

---
+++ Upsampler/dimension_counter.py
@@ -0,0 +1,26 @@
+# here we will count images of each type of dimension and display the most common
+from pprint import pprint
+import numpy
+from PIL import Image
+import pandas
+import os
+import glob
+from collections import Counter
+
+DIR = './input/train2017/'
+
+size_counts = Counter()
+
+for filename in glob.glob(DIR+'*.jpg'): #assuming gif
+    im=Image.open(filename)
+    shape = im.size
+    size_counts[shape] += 1
+
+
+most_common = size_counts.most_common(10)
+
+i = 0
+for (k,v) in most_common :
+    i += 1
+    if i == 10 : break
+    print(f'shape: {k}, count: {v}')
