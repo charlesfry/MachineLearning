476e8b75b507d3827319b657ad7dc76a03c76038
Charles Fry <52051562+AmishWarlord@users.noreply.github.com>
2020-11-01 10:36:17 -0500

    Create torch_ai.py

---
+++ Chess/torch_ai.py
@@ -0,0 +1,16 @@
+import chess
+import torch.nn
+import torch.optim
+import torch.autograd
+import torchvision
+
+import gym
+import math
+import random
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple
+from itertools import count
+from PIL import image
+
