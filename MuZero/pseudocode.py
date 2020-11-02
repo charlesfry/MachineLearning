c2cfd5f107dfc2b8b306ca3cd5f4b1e911881794
Charles Fry <52051562+AmishWarlord@users.noreply.github.com>
2020-11-02 18:08:56 -0500

    Update pseudocode.py

--- MuZero/pseudocode.py
+++ MuZero/pseudocode.py
@@ -354,6 +354,51 @@ class SharedStorage(object):
 ### END OF HELPERS ###
 ######################
 
+# MuZero training is split into two independent parts: Network training and
+# self-play data generation.
+# These two parts only communicate by transferring the latest network checkpoint
+# from the training to the self-play, and the finished games from the self-play
+# to the training.
+def muzero(config: MuZeroConfig):
+    storage = SharedStorage()
+    replay_buffer = ReplayBuffer(config)
+
+    for _ in range(config.num_actors):
+        launch_job(run_selfplay, config, storage, replay_buffer)
+
+    train_network(config, storage, replay_buffer)
+
+    return storage.latest_network()
+
+
+#######################
+## Part 1: Self-Play ##
+
+# Each self-play job is independent of all others; it takes the latest network
+# snapshot, produces a game and makes it available to the training job by
+# writing it to a shared replay buffer.
+def run_selfplay(config:MuZeroConfig, storage: SharedStorage, replay_buffer: ReplayBuffer) :
+    while True :
+        network = storage.latest_network()
+        game = play_game(config, network)
+        replay_buffer.save_game(game)
+
+# each game is produced by starting at the initual board position, then
+# repeatedly executing Monte Carlo Tree Search (MCTS) to generate moves until the end
+# of the game is reached
+def play_game(config: MuZeroConfig, network: Network) -> Game :
+    game = config.new_game()
+
+    while not game.terminal() and len(game.history) < config.max_moves :
+        # at the root of the search tree we use the representation function to
+        # obtain a hidden state given the current observation
+        root = Node(0)
+        current_observation = game.make_image(-1)
+        expand_node(root, game.to_play(), game.legal_actions(), network)
+        action = select_action(config, len(game.history), root, network)
+        game.apply(action)
+        game.store_search_statistics(root)
+        return game
 
 
 
@@ -364,6 +409,14 @@ class SharedStorage(object):
 
 
 
+
+
+######### End Training ###########
+##################################
+
+################################################################################
+############################# End of pseudocode ################################
+################################################################################
 
 # Stubs to make the typechecker happy.
 def softmax_sample(distribution, temperature: float):
