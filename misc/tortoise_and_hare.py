cd27278b62e18a6b1881028606dcee3c9438d5b3
Charles Fry <52051562+AmishWarlord@users.noreply.github.com>
2021-04-08 20:43:40 -0400

    Create tortoise_and_hare.py

---
+++ misc/tortoise_and_hare.py
@@ -0,0 +1,20 @@
+import random
+
+array = list(range(50))
+clean_array = array.copy()
+array.extend([50, 47])
+
+random.shuffle(array)
+random.shuffle(clean_array)
+
+def find_duplicates(arr:array):
+    """
+    if the array contains a duplicate, find it
+    :param arr: the array
+    :return: the duplicate number, or None if no duplicates
+    """
+    if len(arr) < 2: return None
+
+    tortise = arr[0]
+    hare = arr[1]
+
